f89cc3ff509fabf7e0b3300776d72fe515ab92b2
nisol <nisolmedinaue@gmail.com>
2024-06-12 13:37:36 +0200

    all-routes

--- models/Book.model.js
+++ models/Book.model.js
@@ -1,13 +1,24 @@
 const { Schema, model} = require("mongoose");
 
 const bookSchema = new Schema({
-    title:{type: String, require:[true,"title is required"]},
-    author:{type:String, require:[true,"author is required"]},
-    description: {type: String, require:[true, "description is required"]}
+    title:{
+      type: String, 
+      required:[true,"title is required"]
+    },
+    author:{
+      type:String, 
+      required:[true,"author is required"]
+    },
+    description: {
+      type: String, 
+      required:[true, "description is required"]}
 },
 {
    
     timestamps: true
-  });
+  }
+);
+
 const Book = model("Book", bookSchema);
+
 module.exports = Book;
--- routes/books.routes.js
+++ routes/books.routes.js
@@ -1,6 +1,8 @@
+const express =require('express');
 const router = require("express").Router();
 
 const Book = require("../models/Book.model");
+const mongoose =require('mongoose');
 
 
 router.get("/",(req, res, next)=>{
@@ -16,10 +18,14 @@ router.get("/books", (req, res, next) =>{
     });
 });
 
-router.get("/books/:booksId", (req, res, next)=>{
-    const {booksId}= req.params;
-    Book.findById(booksId)
-    .populate(books)
+router.get("/books/:bookId", (req, res, next)=>{
+    const {bookId}= req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(bookId)){
+        return res.status(400).json({message:"specified ID is not valid"});
+    }
+
+    Book.findById(bookId)
     .then((books)=> res.status(200).json(books))
     .catch((error)=>{
         console.log("error while retrieving the book", error);
@@ -38,16 +44,16 @@ router.post("/books", (req, res, next)=>{
     
 });
 
-router.delete("/books/:booksId", (req, res, next)=>{
-    const {booksId}=req.params;
+router.delete("/books/:bookId", (req, res, next)=>{
+    const {bookId}=req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(projectId)) {
-        res.status(400).json({ message: "Specified id is not valid" });
+    if (!mongoose.Types.bookId.isValid(bookId)) {
+        res.status(400).json({ message: "Specified ID is not valid" });
         return;
       }
 
-    Book.findByIdAndDelete(booksId)
-    .then(()=>res.status(200).json({message:`book with ${booksId}is deleted successfully`,}))
+    Book.findByIdAndDelete(bookId)
+    .then(()=>res.status(200).json({message:`book with ${bookId}is deleted successfully`,}))
     .catch((error)=>{
         console.log("Error while deleting the book", error);
         res.status(500).json({message:"Error while deleting the book"});
